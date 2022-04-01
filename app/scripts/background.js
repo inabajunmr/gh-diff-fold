7cd127e7af010efb128f30f861a28129364e38bf
inabajunmr <inabajun.for.regi@gmail.com>
2022-04-01 09:48:54 +0900

    fix problems

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,3 +1,7 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.sendMessage(tab.id, "fold");
+chrome.action.onClicked.addListener(function() {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, 'fold', function(response) {
+		  console.log(response);
+		});
+	  });	
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,11 @@
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-	if (request == "fold") {
-		fold();
-	}
-});
-
 function fold() {
     Array.from(document.getElementsByClassName("file")).forEach(e => {e.classList.remove("open"); e.classList.remove("Details--on");});
 }
+
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		if (request === "fold") {
+			fold();
+		}
+	}
+  );
